625db84c493ba582e8843a166caeaba2ba6da14f
George Beeley <George.Beeley@clue.co.uk>
2021-12-07 20:13:31 +0000

    01SonarSweep complete

---
+++ 01-Sonar_Sweep/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SonarSweep {
+  class Program {
+    static void Main(string[] args) {
+
+      var input = File.ReadLines("problem-input.txt");
+      var increased = 0;
+      var decreased = 0;
+
+      for (int i = 0; i < input.Count(); i++) {
+        if (i > 0 && Int32.Parse(input.ElementAt(i)) > Int32.Parse(input.ElementAt(i-1)))
+          increased++;
+        else
+          decreased++;
+      }
+
+      Console.WriteLine("Increased: " + increased);
+      Console.WriteLine("Decreased: " + decreased);
+    }
+  } 
+}
