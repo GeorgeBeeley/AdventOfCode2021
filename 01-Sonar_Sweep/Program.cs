cd79b4acbd5f1017d15a7ba38a99330bcc5fcb8a
George Beeley <George.Beeley@clue.co.uk>
2021-12-07 21:33:03 +0000

    Day 1 task 2 complete

--- 01-Sonar_Sweep/Program.cs
+++ 01-Sonar_Sweep/Program.cs
@@ -1,20 +1,45 @@
-﻿namespace SonarSweep {
-  class Program {
-    static void Main(string[] args) {
+﻿namespace SonarSweep
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
 
       var input = File.ReadLines("problem-input.txt");
+      var window = 3;
       var increased = 0;
       var decreased = 0;
 
-      for (int i = 0; i < input.Count(); i++) {
-        if (i > 0 && Int32.Parse(input.ElementAt(i)) > Int32.Parse(input.ElementAt(i-1)))
+      int sum = 0, lastSum;
+
+      for (int i = 0; i + window < input.Count(); i++)
+      {
+        lastSum = sum;
+        sum = GetSum(input, i, window);
+
+        if (sum > lastSum)
           increased++;
-        else
+        else if (sum < lastSum)
           decreased++;
+        else
+          Console.WriteLine("no change");
       }
 
       Console.WriteLine("Increased: " + increased);
       Console.WriteLine("Decreased: " + decreased);
     }
-  } 
+
+    static int GetSum(IEnumerable<string> obj, int index, int window)
+    {
+      var sum = 0;
+
+      for (int j = index; j < index + window; j++)
+      {
+        sum += Int32.Parse(obj.ElementAt(j));
+      }
+
+      return sum;
+    }
+    
+  }
 }
