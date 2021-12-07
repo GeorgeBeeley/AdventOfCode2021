ae0ed02fe80b63ce2b501758a64ef25e82041e16
George Beeley <George.Beeley@clue.co.uk>
2021-12-07 22:49:38 +0000

    updated gitignore

---
+++ 01-Sonar_Sweep/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace SonarSweep
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      var input = File.ReadLines("problem-input.txt");
+      var window = 3;
+      var increased = 0;
+      var decreased = 0;
+
+      int sum = 0, lastSum;
+
+      for (int i = 0; i + window < input.Count(); i++)
+      {
+        lastSum = sum;
+        sum = GetSum(input, i, window);
+
+        if (sum > lastSum)
+          increased++;
+        else if (sum < lastSum)
+          decreased++;
+        else
+          Console.WriteLine("no change");
+      }
+
+      Console.WriteLine("Increased: " + increased);
+      Console.WriteLine("Decreased: " + decreased);
+    }
+
+    static int GetSum(IEnumerable<string> obj, int index, int window)
+    {
+      var sum = 0;
+
+      for (int j = index; j < index + window; j++)
+      {
+        sum += Int32.Parse(obj.ElementAt(j));
+      }
+
+      return sum;
+    }
+    
+  }
+}
